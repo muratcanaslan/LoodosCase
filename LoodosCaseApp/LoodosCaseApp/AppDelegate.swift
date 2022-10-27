56ab4434f2eced96e9c2249d1b48545989bbc79c
Murat Can ASLAN <>
2022-10-28 02:24:06 +0300

    refactor(modules): Configuration project for initial setup

---
+++ LoodosCaseApp/LoodosCaseApp/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  LoodosCaseApp
+//
+//  Created by Murat Can ASLAN on 28.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        vc.view.backgroundColor = .red
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ LoodosCaseApp/LoodosCaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosCaseApp
+//
+//  Created by Murat Can ASLAN on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

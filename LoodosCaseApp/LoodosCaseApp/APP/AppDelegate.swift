af2fa342069328abe774c901b84af7e0f37fedd1
Murat Can ASLAN <>
2022-10-28 02:59:38 +0300

    refactor(modules-splash): Create Splash screen and configure app delegate for window screen

--- LoodosCaseApp/LoodosCaseApp/APP/AppDelegate.swift
+++ LoodosCaseApp/LoodosCaseApp/APP/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = UIViewController()
-        vc.view.backgroundColor = .red
+        let vc = SplashVC()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
         return true
---
+++ LoodosCaseApp/LoodosCaseApp/Modules/Splash/SplashVC.swift
@@ -0,0 +1,19 @@
+//
+//  SplashVC.swift
+//  LoodosCaseApp
+//
+//  Created by Murat Can ASLAN on 28.10.2022.
+//
+
+import UIKit
+
+final class SplashVC: UIViewController {
+
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var messageButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
